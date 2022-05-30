4617ef266ab2fa27c98e7ab63ee3fc2308a209ea
WolfyewParrot <106536304+WolfyewParrot@users.noreply.github.com>
2022-05-30 20:05:45 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Vẹt có yêu Wolf ko ?",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+    "Trả lời đi và đừng thoát ra",
+  buttonYes: "Có yêu",
+  buttonNo: "Không yêu ! Ghét",
+  titleModar: "Yew Vẹt rất nhiều",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Wolf biết là Vẹt sẽ yêu Wolf mà <3 </br> Tớ có 1 câu thơ dành tặng cậu :333 </br> "Có một điều mà cậu nên biết là </br>  Bệnh nào cũng có thuốc chữa nhưng duy nhất và sẽ không khi nào có thuốc chữa được bệnh tớ ngừng thik cậu "',
 };
